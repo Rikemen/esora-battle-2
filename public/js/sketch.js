81f721e1de4e1fe1daeb12127c72ae4231b728bf
Rikemen <rikemen.rikejo@gmail.com>
2024-11-14 20:40:56 +0900

    index.htmlのワイヤーと各ページへのリンク設置、help.htmlの追加

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -4,13 +4,13 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
+  // createCanvas(windowWidth, windowHeight);
+  // background(255);
 
 }
 
 
 function draw() {
-  fill(175);
-  ellipse(width/2, height/2, 100, 100);
+  // fill(175);
+  // ellipse(width/2, height/2, 100, 100);
 }
